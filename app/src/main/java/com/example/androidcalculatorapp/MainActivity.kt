fa7fb8ebd4674a2550b9b13ede5c33f543516463
ChaminduWN <127916715+chamindu2001@users.noreply.github.com>
2024-06-16 02:11:47 +0530

    add idsdd

--- app/src/main/java/com/example/androidcalculatorapp/MainActivity.kt
+++ app/src/main/java/com/example/androidcalculatorapp/MainActivity.kt
@@ -7,7 +7,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_main)
 
-        //kkkkkkkkkkkkkk
+//ddd
+        //dd
     }
 }
