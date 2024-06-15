6cf2035267372e8028c1ae46f129b2402adcad71
ChaminduWN <127916715+chamindu2001@users.noreply.github.com>
2024-06-16 02:13:03 +0530

    correct main kt file

--- app/src/main/java/com/example/androidcalculatorapp/MainActivity.kt
+++ app/src/main/java/com/example/androidcalculatorapp/MainActivity.kt
@@ -7,11 +7,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            setContentView(R.layout.activity_main)
 
-//ddd
-        //dd
+
     }
 }
